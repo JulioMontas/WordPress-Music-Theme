2f41e6ea8b4d182c709218c305d9bf9813e1ec9e
NULL NULL <null@NULLs-MacBook-Pro.local>
2017-02-06 21:36:27 -0500

    Added no title page and pages class name

---
+++ page_biography.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Sonia_Choi_Music
+ */
+
+/*
+	Template Name: Biography
+*/
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main biography-pagina" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content-page-no-title', 'page' );
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<script>
+	function openTap(tapName) {
+	    var i;
+	    var x = document.getElementsByClassName("tap");
+	    for (i = 0; i < x.length; i++) {
+	       x[i].style.display = "none";
+	    }
+	    document.getElementById(tapName).style.display = "block";
+	}
+	</script>
+
+<?php
+get_footer();
